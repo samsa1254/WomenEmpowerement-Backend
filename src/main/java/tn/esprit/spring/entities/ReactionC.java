1e1984da22322eba1d75804ee30b36ac0e9668a8
mathlouthi dhia <dhia.mathlouthi@esprit.tn>
2022-02-22 00:00:35 +0100

    ggg

--- src/main/java/tn/esprit/spring/entities/ReactionC.java
+++ src/main/java/tn/esprit/spring/entities/ReactionC.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
@@ -32,6 +33,7 @@ public class ReactionC {
 	private String ValueC ; 
 	
 	@ManyToOne
+	@JsonIgnore
 	private CommentD commentd ;
 
 }
